e3f364cf8c5f6f3ee4985bb091f5d8e0c2f00c6a
alimr713971-wq <alimr713971@gmail.com>
2025-08-29 07:19:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function callAPI() {
   try {
-    const response = await fetch("https://followmeviews-backend.vercel.app/api/hello"); 
+    const response = await fetch("https://followmeviews-api-2uuj.vercel.app/api/hello");
     const data = await response.json();
     document.getElementById("result").innerText = JSON.stringify(data);
   } catch (error) {
